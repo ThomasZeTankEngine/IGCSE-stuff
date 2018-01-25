5161490a665745ddeb43114a0d7eaf919e4585f6
Dung <r00702@bvisrc.net>
2018-01-25 14:30:33 +0700

    First

---
+++ Project1.py
@@ -0,0 +1,3 @@
+lunches = ["potato thing", "noodles", "rice", "beef", "chicken ", "soup", "mice"]
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+print(lunches[1])
