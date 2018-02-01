26892d58860171905509af1c4ab7dd7bd4464fbe
Dung <r00702@bvisrc.net>
2018-02-01 15:02:17 +0700

    First

---
+++ Grading program.py
@@ -0,0 +1,66 @@
+student = int(input("What is the number of students?"))
+name = []
+score = []
+
+for i in range(student):
+    name.append(str((input("Gimme a name"))))
+    score.append(int(input("Gimme their score")))
+
+for i in range(len(score)):
+    max = score[0]
+    if score[i]>max:
+        max = score[i]
+
+position = score.index(max)
+print(name[max])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
